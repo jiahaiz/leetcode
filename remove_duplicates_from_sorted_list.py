9fd9832b3c309d5f126925668797c526b51c7065
N635053 <jiahai.zhang@jpmchase.com>
2017-12-27 11:39:26 -0500

    add code

--- remove_duplicates_from_sorted_list.py
+++ remove_duplicates_from_sorted_list.py
@@ -6,10 +6,10 @@ Created on Wed Dec 27 10:09:58 2017
 """
 
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+     def __init__(self, x):
+         self.val = x
+         self.next = None
 
 class Solution(object):
     def deleteDuplicates(self, head):
@@ -17,4 +17,29 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
+        move = head
+        while(move!=None):
+            if(move.next != None):
+                if(move.next.val==move.val):
+                    move.next = move.next.next
+                else:
+                    move = move.next
+            else:
+                break
+        return head
         
+def prt(a):
+    while(a!= None):
+     print a.val
+     a= a.next       
+       
+if __name__ == "__main__":
+    s = Solution()
+    a= ListNode(1)
+    b= ListNode(1)
+    c= ListNode(1)
+    b.next = c
+    a.next=b
+    s.deleteDuplicates(c) 
+    prt(c) 
+    
