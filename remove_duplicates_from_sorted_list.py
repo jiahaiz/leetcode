629652e7b23a80ca5e2fc538815a369e26b962d8
N635053 <jiahai.zhang@jpmchase.com>
2017-12-27 10:27:22 -0500

    test in commit in cmd

---
+++ remove_duplicates_from_sorted_list.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 27 10:09:58 2017
+
+@author: N635053
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
